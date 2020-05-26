796ffcb21c28c8212cdc0f20539d7fa9e6a58157
Ayberk <ayberkersoy@gmail.com>
2020-05-26 15:25:38 +0300

    Add status column to products table

--- app/Category.php
+++ app/Category.php
@@ -8,7 +8,7 @@ class Category extends Model
 {
     public function products()
     {
-        return $this->hasMany(Product::class);
+        return $this->hasMany(Product::class)->where('status', Product::Available);
     }
 
     public function image()
