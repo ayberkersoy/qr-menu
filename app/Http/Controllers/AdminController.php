cf5d3bea7624870c5236c5e39ffa43d355762c9c
Ayberk <ayberkersoy@gmail.com>
2020-06-01 14:16:07 +0300

    Edit admin index view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.home');
+        $orders = Order::where('status', Order::New)->orderBy('created_at', 'desc')->take(5)->get();
+        return view('admin.home', compact('orders'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -11,13 +11,13 @@
             <!-- small box -->
             <div class="small-box bg-aqua">
                 <div class="inner">
-{{--                    <h3>{{  }}</h3>--}}
-                    <p>Sipariş</p>
+                    <h3>{{ \App\Order::where('status', \App\Order::New)->count() }}</h3>
+                    <p>Bekleyen Sipariş</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="/orders" class="small-box-footer">Tümü <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="/admin/orders?status=100" class="small-box-footer">Tümü <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -25,13 +25,13 @@
             <!-- small box -->
             <div class="small-box bg-green">
                 <div class="inner">
-{{--                    <h3>{{ reservations.length }}</h3>--}}
-                    <p>Rezervasyon</p>
+                    <h3>{{ \App\Order::where('status', \App\Order::Sent)->count() }}</h3>
+                    <p>Onaylanan Sipariş</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-calendar"></i>
                 </div>
-                <a href="/reservations" class="small-box-footer">Tümü <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="/admin/orders?status=200" class="small-box-footer">Tümü <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -39,13 +39,13 @@
             <!-- small box -->
             <div class="small-box bg-yellow">
                 <div class="inner">
-{{--                    <h3>{{ users.length }}</h3>--}}
-                    <p>Kullanıcı</p>
+                    <h3>{{ \App\Category::count() }}</h3>
+                    <p>Kategori</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-person-add"></i>
                 </div>
-                <a href="/users" class="small-box-footer">Tümü <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="{{ route('categories.index') }}" class="small-box-footer">Tümü <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -53,13 +53,13 @@
             <!-- small box -->
             <div class="small-box bg-red">
                 <div class="inner">
-{{--                    <h3>{{ products.length }}</h3>--}}
+                    <h3>{{ \App\Product::count() }}</h3>
                     <p>Ürün</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-pizza"></i>
                 </div>
-                <a href="/products" class="small-box-footer">Tümü <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="{{ route('products.index') }}" class="small-box-footer">Tümü <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -71,30 +71,27 @@
                 <!-- /.box-header -->
                 <div class="box-body table-responsive no-padding">
                     <table class="table table-bordered table-striped table-hover data-table">
+                        <thead>
                         <tr>
-                            <th>#</th>
-                            <th>Sipariş Tarihi</th>
-                            <th>Sipariş Sahibi</th>
-                            <th>Sipariş Tutarı</th>
-                            <th>Sipariş Durumu</th>
+                            <th>Masa Numarası</th>
+                            <th>Tutar</th>
+                            <th>Tarih</th>
                             <th>İşlemler</th>
                         </tr>
-                        <tr>
-                            <td>{{ 'orderLast.id' }}</td>
-                            <td>{{ 'orderLast.date_ordered' }}</td>
-                            <td>{{ 'orderLast.user.name' }} {{ 'orderLast.user.surname' }}</td>
-                            <td>{{ 'orderLast.price' }}</td>
-                            <td>
-                                <span class="label label-danger">İptal Edildi</span>
-                                <span class="label label-warning">Bekliyor</span>
-                                <span class="label label-primary">Onaylandı</span>
-                                <span class="label label-success">Gönderildi</span>
-                            </td>
-                            <td>
-                                <a href="#" class="btn btn-info"><i class="fa fa-eye"></i></a>
-                                <!-- <a href="#" class="btn btn-xs btn-danger" v-on:click="handleSubmit(campaign.id, index)"><i class="fa fa-trash-o"></i></a> -->
-                            </td>
-                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($orders as $order)
+                            <tr>
+                                <td>{{ $order->table_no }}</td>
+                                <td>{{ $order->total }}</td>
+                                <td>{{ $order->created_at->format('d/m/Y H:i') }}</td>
+                                <td>
+                                    <!-- <a href="#" class="btn btn-xs btn-danger" v-on:click="handleSubmit(campaign.id, index)"><i class="fa fa-trash-o"></i></a> -->
+                                    <a href="{{ route('orders.show', $order->id) }}" class="btn btn-info"><i class="fa fa-eye"></i></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
                     </table>
                 </div>
                 <!-- /.box-body -->
